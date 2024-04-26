01061c168f7c449ccb21d554bc90417599081c1c
Larissa Gramacho <ggramacholarissa@gmail.com>
2024-04-26 17:45:54 -0300

    adicionando estrutura do projeto

--- cypress/e2e/user_registration.cy.js
+++ cypress/e2e/user_registration.cy.js
@@ -1,5 +1,48 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import './commands'
+import { faker } from '@faker-js/faker'
+
+
+describe('user registration', () => {
+
+
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
   })
-})
+
+  it('sucessful user registration', () => {
+
+
+    cy.new_user(faker.person.fullName(), faker.internet.email(), faker.internet.password());
+
+
+  })
+
+  it('Display feedback when all required fields are not completed', () => {
+
+
+    cy.display_feedback_all();
+
+
+  })
+
+
+  it('Display feedbak when passowrds do not match', () => {
+
+
+    cy.display_feedback_passwords_do_not_match();
+
+
+  })
+
+  it('display feedback when password does not meet requirements', () => {
+
+
+    cy.display_feedback_password();
+
+
+  })
+
+
+}
+
+)
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,48 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import './elements'
+
+
+Cypress.Commands.add('new_user', (name, email, password) => {
+
+    cy.get(elem.btn_link_register).click();
+
+    cy.get(elem.name).type(name);
+    cy.get(elem.email).type(email);
+    cy.get(elem.password).type(password);
+    cy.get(elem.password).type(password);
+
+    cy.get(elem.btn_register).click();
+
+
+})
+
+
+Cypress.Commands.add('display_feedback_all', () => {
+
+    cy.get(elem.btn_link_register).click();
+    cy.get(elem.btn_register).click();
+    cy.contains('É necessário informar um endereço de email').should('be.visible');
+    cy.contains('Crie uma senha').should('be.visible');
+    cy.contains('Repita a senha criada acima').should('be.visible');
+
+
+})
+
+
+Cypress.Commands.add('display_feedback_password', (name, email, password) => {
+
+    cy.get(elem.btn_link_register).click();
+    cy.get(elem.name).type(name);
+    cy.get(elem.email).type(email);
+    cy.get(elem.password).type(password);
+    cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible');
+})
+
+Cypress.Commands.add('display_feedback_passwords_do_not_match', (name, email, password) => {
+
+    cy.get(elem.btn_link_register).click();
+    cy.get(elem.name).type(name);
+    cy.get(elem.email).type(email);
+    cy.get(elem.password).type(password);
+    cy.get(elem.password).type('abrobrinha123');
+    cy.contains('As senhas não batem').should('be.visible');
+})
